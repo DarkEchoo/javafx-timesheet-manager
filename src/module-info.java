2b8d54871735ffb927f0842872d232f3090b7300
Tropic <78857605+DarkEchoo@users.noreply.github.com>
2026-05-20 16:24:18 -0600

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module com.jesse.timesheet {
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    
+    requires java.desktop;
+
+    exports com.jesse.timesheet;
+}
